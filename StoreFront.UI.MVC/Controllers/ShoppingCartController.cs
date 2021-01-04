cee21ab5e20d68470b6328b0c525be2197e3fda6
David See <engr12b10@hotmail.com>
2021-01-03 19:23:10 -0600

    added shopping cart functionality

---
+++ StoreFront.UI.MVC/Controllers/ShoppingCartController.cs
@@ -0,0 +1,31 @@
+﻿using StoreFront.UI.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            var shoppingCart = (Dictionary<int, CartItemViewModel>)Session["cart"];
+
+            if (shoppingCart == null || shoppingCart.Count == 0)
+            {                
+                shoppingCart = new Dictionary<int, CartItemViewModel>();               
+                ViewBag.Message = "There are no items in your cart.";
+            }
+            else
+            {
+                ViewBag.Message = null;
+            }
+
+            return View(shoppingCart);
+        }
+    }
+    
+}
---
+++ StoreFront.UI.MVC/Models/CartItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StoreFront.DATA.EF;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreFront.UI.MVC.Models
+{
+    public class CartItemViewModel
+    {
+        [Range(1, int.MaxValue)]
+        public int Qty { get; set; }
+        public Balloon Product { get; set; }
+
+        public CartItemViewModel(int qty, Balloon product)
+        {
+            //property = parameter            
+            Product = product;
+            Qty = qty;
+        }
+    }
+}
